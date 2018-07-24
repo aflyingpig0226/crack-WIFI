2dea22007b946bec8f8eae36af9a31591a5704a0
石锐 <32090632+ruibababa@users.noreply.github.com>
2018-07-24 14:53:28 +0800

    Update detection_wifi.py

--- detection_wifi.py
+++ detection_wifi.py
@@ -3,13 +3,13 @@ import pywifi
 from pywifi import const
 
 
-class DetectionOfWifi:
+class DetectionOfWifi:  # 创建扫描wifi的对象
 
 	def gic(self):
 
-		wifi = pywifi.PyWiFi()
-		ifaces = wifi.interfaces()[0]
-		if ifaces.status() == const.IFACE_CONNECTED:
+		wifi = pywifi.PyWiFi()  # 获取网卡
+		ifaces = wifi.interfaces()[0]  # 获取网卡的接口
+		if ifaces.status() == const.IFACE_CONNECTED:  # 判断时候WiFi已经连接
 
 			print("已经连接。")
 
@@ -19,17 +19,17 @@ class DetectionOfWifi:
 
 	def bies(self):
 
-		wifi = pywifi.PyWiFi()
-		ifaces = wifi.interfaces()[0]
-		ifaces.scan()
-		result = ifaces.scan_results()
+		wifi = pywifi.PyWiFi()  # 获取网卡
+		ifaces = wifi.interfaces()[0]  # 获取网卡的接口
+		ifaces.scan()  # 扫描周围的wifi信号
+		result = ifaces.scan_results()  # 接收扫描的结果
 		for name in result:
 
-			print(name.ssid)
+			print(name.ssid)  # 遍历扫描的结果
 
 
 if __name__ == "__main__":
 
 	wifi = DetectionOfWifi()
 	wifi.gic()
-	wifi.bies()
+	wifi.bie()
